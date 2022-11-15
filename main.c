bf2854e4703a1c312f8d75a91b57882d6c327a62
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-11-15 22:05:32 +0900

    실습3

--- main.c
+++ main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*void swap (int x, int y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
 void main(void) {
-	int i = 4; //300으로 변경해서도 해보기
-	
-	int *pi = &i;
-	char *pc = &i;
-	
-	printf("%i, %i, %i\n", i, *pi, *pc);
-	
-	return 0;
+	int a = 3;
+	int b = 5;
+	swap(a, b);
+	printf("a:%i, b:%i\n", a, b);
+}
+*/
+void swap (int *x, int *y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+void main(void) {
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a, b);
 }
